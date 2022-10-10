44c9c3470147e695236c97e62be1574da5e9f6ab
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2022-10-10 12:40:58 +0800

    Add project files.

---
+++ console-app-fcc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app_fcc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Logging output to the console
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            // Pause the console without closing
+            Console.ReadLine();
+        }
+    }
+}
