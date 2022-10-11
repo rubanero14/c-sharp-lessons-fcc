9b6dd2ea0f5027eb5198b11ee304c9aa3356f171
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-11 21:54:52 +0800

    comparison using if statements in C#

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,25 +10,28 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            bool isMale = true;
-            bool isTall = true;
-
-            if (isMale && isTall)
-            {
-                Console.WriteLine("You are tall male!");
-            } else if (isMale && !isTall)
-            {
-                Console.WriteLine("You are short male!");
-            } else if (!isMale && isTall)
-            {
-                Console.WriteLine("You are tall female!");
-            } else
-            {
-                Console.WriteLine("You are short female!");
-            }
+            Console.WriteLine(GetMax(20, 10, 50));
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            } else
+            {
+                result = num3;
+            }
+
+            return result;
         }
     }
 }
