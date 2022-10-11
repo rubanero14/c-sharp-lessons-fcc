6b9c04ddd1fbf4efbe5930fee46097b26f1756ef
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-11 19:31:25 +0800

    Method's Typed Return Statement

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -12,22 +12,16 @@ namespace console_app_fcc
         {
             // Calling here the method constructed below the function Main
             // Whenever code is compiled and executed, the system only runs any code stated within Main method
-            SayHi();
-            SayHi();
-            SayHi();
+            int cubedNumber = Cube(5);
+            Console.WriteLine(cubedNumber);
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
         }
 
-        // Creating new method, static keyword used to invoke a method that returns something, keyword void is to maintain return 'nothing', followed by the method name with Cap letter on each word
-        static void SayHi(/*string name*/) {
-            Console.Write("Insert your name here: ");
-            string name = Console.ReadLine();
-            Console.Write("Insert your age here: ");
-            string age = Console.ReadLine();
-
-            Console.WriteLine("Hello {0} and your age is {1}!", name, age);
+        // Creating new method, static keyword used to invoke a method that returns something, keyword double is to maintain return double value type, followed by the method name with Cap letter on each word
+        static int Cube(int measurement) {
+            return measurement * measurement * measurement;
         }
     }
 }
