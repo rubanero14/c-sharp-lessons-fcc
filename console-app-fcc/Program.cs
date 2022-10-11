7290476897f86787e8691631b2fac104cfa874c1
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-11 11:07:00 +0800

    Working with Strings

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,14 +10,17 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Learning different data types
-            string phrase = "Hello";
-            char grade = 'A';
-            int age = 36;
-            double gpa = 4.0;
-            bool isMale = true;
+            string phrase = "Learn C#" + " is Cool!";
 
-            Console.WriteLine("{0} {1} {2} {3} {4}", phrase, grade, age, gpa, isMale);
+            // Working with strings
+            Console.WriteLine("{0} \n \" Hi There", phrase);
+            Console.WriteLine("{0} to check length of string use .Length()", phrase.Length);
+            Console.WriteLine("{0} => To Upper Case \n{1} => To Lower Case", phrase.ToUpper(), phrase.ToLower());
+            Console.WriteLine("{0} => Contains C# substring \n{1} => Contains CSharp substring", phrase.Contains("C#"), phrase.Contains("CSharp"));
+            Console.WriteLine("{0} => first char in string \n{1} => middle char in string \n{2} => last char in string", phrase[0], phrase[((phrase.Length - 1)/2)], phrase[phrase.Length - 1]);
+            Console.WriteLine("{0} => finding if a char exist and its position \n{1} If a char or string dont exist returns -1", phrase.IndexOf("earn"), phrase.IndexOf("xavier"));
+            Console.WriteLine("{0} {1} => .Substring accepts index and returns the char or word from the position stated and second arguement is to extract and show how many chars",
+                phrase.Substring(phrase.IndexOf("C#"), 4), phrase.IndexOf("C#"));
 
             // Pause the console without closing
             Console.ReadLine();
