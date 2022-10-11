4724a1b7582f900a9b7334df7c15ea6039c4f8c0
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-11 22:06:23 +0800

    Building a Better Calculator (Using If Statements)

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,28 +10,35 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetMax(20, 10, 50));
+            // Advanced calculator
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            } else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            } else if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            } else
+            {
+                Console.WriteLine("Invalid operator!");
+            }
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
-        }
-
-        static int GetMax(int num1, int num2, int num3)
-        {
-            int result;
-
-            if (num1 >= num2 && num1 >= num3)
-            {
-                result = num1;
-            } else if (num2 >= num1 && num2 >= num3)
-            {
-                result = num2;
-            } else
-            {
-                result = num3;
-            }
-
-            return result;
         }
     }
 }
