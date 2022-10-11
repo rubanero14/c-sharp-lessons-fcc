9d4dea36ed60cec633c63725d5665a4a6c1c4571
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-11 19:58:58 +0800

    If Statements in C#

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,18 +10,25 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Calling here the method constructed below the function Main
-            // Whenever code is compiled and executed, the system only runs any code stated within Main method
-            int cubedNumber = Cube(5);
-            Console.WriteLine(cubedNumber);
+            bool isMale = true;
+            bool isTall = true;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are tall male!");
+            } else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are short male!");
+            } else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are tall female!");
+            } else
+            {
+                Console.WriteLine("You are short female!");
+            }
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
-        }
-
-        // Creating new method, static keyword used to invoke a method that returns something, keyword double is to maintain return double value type, followed by the method name with Cap letter on each word
-        static int Cube(int measurement) {
-            return measurement * measurement * measurement;
         }
     }
 }
