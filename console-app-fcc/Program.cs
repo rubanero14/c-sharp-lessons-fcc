96f31351ae5189e949f8b73afb1f5490fc9dba4e
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 15:02:50 +0800

    Comments in C#

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,26 +10,13 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Adding , inside of type array declaration, signifies that this variable is 2 dimensional array
-            // for more dimension, just add more ,
-            // Example for 3D array, int[,,] is used to declared
-            int[,] numberGrid = {
-                {1, 2}, 
-                {3, 4}, 
-                {5, 6},
-            };
-
-            // for unknown values in the array, we initialize the array as below, where 3 signifies number of rows and 2 signifies number of columns inside each rows
+            // Comments in C#
+            // Single Line Comments
             /*
-               equivalent to int[,] myArray = {
-                    {A, B}, 
-                    {C, D}, 
-                    {E, F}
-               }; 
+                Multi
+                Lines
+                Comment
              */
-            int[,] myArray = new int[3,2];
-
-            Console.WriteLine(numberGrid[1, 1]);
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
