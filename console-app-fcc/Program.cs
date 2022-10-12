14e4dff7773abb7a0fb85cd57c4ce7467aa7b7d7
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 11:23:54 +0800

    While Loops

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,46 +10,16 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Initiate GetDay fn here
-            Console.WriteLine(GetDay(7));
+            // Basic while loop
+            int index = 6;
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
-        }
-
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch(dayNum) 
-            {
-                case 0:
-                    dayName = "Sunday";
-                    break;
-                case 1:
-                    dayName = "Monday";
-                    break;
-                case 2:
-                    dayName = "Tuesday";
-                    break;
-                case 3:
-                    dayName = "Wednesday";
-                    break;
-                case 4:
-                    dayName = "Thursday";
-                    break;
-                case 5:
-                    dayName = "Friday";
-                    break;
-                case 6:
-                    dayName = "Saturday";
-                    break;
-                default:
-                    dayName = "Invalid Day Number";
-                    break;
-            };
-
-            return dayName;
         }
     }
 }
