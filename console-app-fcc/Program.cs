c9a96a006045ffc83d64aee00918df09b36c797c
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 14:09:15 +0800

    2D Arrays

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,23 +10,29 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Building Exponent function
-            Console.WriteLine(GetPower(4,3));
+            // Adding , inside of type array declaration, signifies that this variable is 2 dimensional array
+            // for more dimension, just add more ,
+            // Example for 3D array, int[,,] is used to declared
+            int[,] numberGrid = {
+                {1, 2}, 
+                {3, 4}, 
+                {5, 6},
+            };
+
+            // for unknown values in the array, we initialize the array as below, where 3 signifies number of rows and 2 signifies number of columns inside each rows
+            /*
+               equivalent to int[,] myArray = {
+                    {A, B}, 
+                    {C, D}, 
+                    {E, F}
+               }; 
+             */
+            int[,] myArray = new int[3,2];
+
+            Console.WriteLine(numberGrid[1, 1]);
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
-        }
-
-        static double GetPower(int baseNum, int powerNum)
-        {
-            double result = baseNum;
-
-            for (int i = 1; i < powerNum; i++)
-            {
-                result = result * baseNum;
-            }
-
-            return result;
         }
     }
 }
