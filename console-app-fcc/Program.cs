3fe39cecea2fb1fe076ac9624b3179f82aa3cb09
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 17:06:45 +0800

    Object Methods

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,27 +10,13 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Creating a new object using Book class, a new instance of a Book class
-            // Notice the type declared is type of Book which is custom data type declared in the Book Class, followed by the variable name and storing the new Book() object in it
-            // Defining the new books properties by passing as arguements
-            Book book1 = new Book("Harry Potter", "J.K. Rowling", 400);
+            Student student1 = new Student("Sri", "Computer Science", 4.00m);
+            Student student2 = new Student("Sharm", "Business", 3.95m);
+            Student student3 = new Student("Raj", "Business", 3.45m);
 
-            Console.WriteLine(book1.title);
-            Console.WriteLine(book1.author);
-            Console.WriteLine(book1.pages);
-            Console.WriteLine();
-
-            Book book2 = new Book("Lord of the Rings", "J.R.R. Tolkien", 700);
-
-            // Overwriting the existing title attribute of the book2 object
-            book2.title = "The Hobbit";
-
-            Console.WriteLine(book2.title);
-            Console.WriteLine(book2.author);
-            Console.WriteLine(book2.pages);
-
-            // Creating a new book3 object without any arguements passing into it
-            Book book3 = new Book();
+            Console.WriteLine(student1.hasHonors());
+            Console.WriteLine(student2.hasHonors());
+            Console.WriteLine(student3.hasHonors());
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
---
+++ console-app-fcc/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app_fcc
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public decimal gpa;
+
+        public Student(string Name, string Major, decimal Gpa)
+        {
+            name = Name;
+            major = Major;
+            gpa = Gpa;
+        }
+
+        // Custom Object Method to determine whether a student has Honors if student's GPA is 3.5 and above
+        public bool hasHonors()
+        {
+            if(gpa >= 3.50m)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
