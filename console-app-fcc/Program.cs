f04cb066766aa3ec9046ad3f342f71d15f2567c6
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 15:26:38 +0800

    Exception/Error Handling

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,14 +10,40 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Comments in C#
-            // Single Line Comments
-            /*
-                Multi
-                Lines
-                Comment
-             */
+            // try catch block used to handle exceptions/errors
+            // try block is used to run risky codes that might throw an exception or errors
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine(num1 / num2);  
+            }
+            // catch block is fired when an error is encountered by running try block
+            // Catching All Exceptions
+            /*
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            */
+            // Catching Specific Exception
+            catch (DivideByZeroException e) // Exception keyword handles all exceptions, DivideByZeroException only handles divide by zero errors
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            catch (FormatException e) // FormatException only handles errors made by type or operations based errors, ie: dividing number using chars
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            // finally block is fired no matter if an exception thrown or not
+            finally
+            {
+                Console.WriteLine("This fires all the time whenever a try block or catch block code are run!");
+            }
+            
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
         }
