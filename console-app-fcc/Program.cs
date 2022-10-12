bb4211938c78d7e23d9fbe89fd2cc5904961f095
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 13:38:13 +0800

    For Loop

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,37 +10,15 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            string secretWord = "giraffe";
-            string guess = "";
-            int guessCount = 0;
-            int guessLimit = 3;
-            bool outOfGuesses = false;
-
-            do
+            int[] oddNumbers = {1, 3, 6, 9, 12, 15, 18, 21};
+            for (int i = 0; i < oddNumbers.Length; i++)
             {
-                if (guessCount < guessLimit)
-                {
-                    Console.WriteLine("Your guess so far => {0}", guess.Length > 0 ? guess : "none" );
-                    Console.Write("Enter the guess: ");
-                    guess = Console.ReadLine();
-                    guessCount++;
-                } else
-                {
-                    outOfGuesses = true;
-                }
-            }  while (guess != secretWord  && !outOfGuesses);
+                Console.WriteLine(oddNumbers[i]);
+            }
 
-            switch(outOfGuesses)
+            for (int j = 1; j <= 5; j++)
             {
-                case true:
-                    Console.Write("You Lose!");
-                    break;
-                case false:
-                    Console.Write("You Win!");
-                    break;
-                default:
-                    Console.WriteLine("Game has started!");
-                    break;
+                Console.WriteLine(j);
             }
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
