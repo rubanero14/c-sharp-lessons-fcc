4335f99c01b006eac5a49a461a88d6c90c944a34
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-12 11:09:02 +0800

    Switch statement in C#

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,35 +10,46 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            // Advanced calculator
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter an operator: ");
-            string op = Console.ReadLine();
-
-            Console.Write("Enter another number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            } else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            } else if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            } else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            } else
-            {
-                Console.WriteLine("Invalid operator!");
-            }
+            // Initiate GetDay fn here
+            Console.WriteLine(GetDay(7));
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch(dayNum) 
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            };
+
+            return dayName;
         }
     }
 }
