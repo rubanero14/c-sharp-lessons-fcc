c9438372e81d9cf83a1649d39b3db1cf91fd0b81
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-13 10:40:53 +0800

    Static Methods & Classes

--- console-app-fcc/Program.cs
+++ console-app-fcc/Program.cs
@@ -10,18 +10,8 @@ namespace console_app_fcc
     {
         static void Main(string[] args)
         {
-            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
-
-            // accessing the static attribute of the class
-            Console.WriteLine(Song.songCount);
-
-            Song cholaChola = new Song("Chola Chola", "A.R. Rahman", 200);
-
-            // accessing the static attribute of the class using type/class
-            Console.WriteLine(Song.songCount);
-
-            // accessing the static attribute of the class using object
-            Console.WriteLine(kashmir.GetSongCount());
+            // Accessing static method in a class
+            UsefulTools.SayHi("Raj");
 
             // Console.ReadLine() acts as prompt that watches, reads and returns for user input as string and also pauses the console without closing
             Console.ReadLine();
---
+++ console-app-fcc/UsefulTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app_fcc
+{
+    // adding static modifier to disable user from creating new object if this class, only accessible via calling static method only
+    static class UsefulTools
+    {
+        // Creating a static method
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello {0}!", name);
+        }
+    }
+}
